14761019c0d02a583bcb8b2d07caa41f679933fe
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-01-31 01:09:52 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
+import "./App.css";
 import React from "react";
-import { Input, Form } from "antd";
+import { Input, Form, Button } from "antd";
 import Dev_img from "./images/dev_img.png";
 
 export default function SignUp() {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <h1>Sign Up</h1>
-            <p>And enjoy life during the time just you saved</p>
+      <div className="container my-5 background_main">
+        <div className="row MainDiv">
+          <div className="col-md-6 d-flex justify-content-center align-items-center">
             <div>
-              <a href="/">Sign Up with Google</a>
-              <a href="/">Sign Up with Facebook</a>
+              <h1>Sign Up</h1>
+              <p>And enjoy life during the time just you saved</p>
+              <div>
+                <a href="/">Sign Up with Google</a>
+                <a href="/">Sign Up with Facebook</a>
+              </div>
+              <Form.Item
+                label="Name"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Name!",
+                    type: "text",
+                  },
+                ]}
+              >
+                <Input placeholder="Example" />
+              </Form.Item>
+              <Form.Item
+                label="Email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Email!",
+                    type: "email",
+                  },
+                ]}
+              >
+                <Input placeholder="info@example.com" />
+              </Form.Item>
+              <Form.Item
+                label="Password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+              >
+                <Input.Password placeholder="8+ Characters, 1 Capital letters" />
+              </Form.Item>
+              <Button>Create an Account</Button>
             </div>
-            <Form.Item
-              label="Email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your password!",
-                  type: "email",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Password"
-              rules={[
-                { required: true, message: "Please input your password!" },
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
           </div>
-          <div className="col-md-6">
+          <div className="col-md-6 d-flex justify-content-center align-items-center">
             <img src={Dev_img} alt="Dev_img" />
           </div>
         </div>
